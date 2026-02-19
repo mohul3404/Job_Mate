5d7d5dd3803cd104fa5d053b6b3e572cbd5af4eb
mohul3404 <bansalmohul@gmail.com>
2026-02-19 09:04:30 +0530

    fix api urls for production

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/login`,
+        "http://localhost:4000/api/v1/user/login",
         { email, password, role },
         {
           headers: {
